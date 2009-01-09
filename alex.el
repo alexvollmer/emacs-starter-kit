f29bef05e9c9afd14ca53745663524b19fe3a496
Alex Vollmer <alex.vollmer@gmail.com>
2009-01-08 20:24:48 -0800

    Figured out how to turn the damn flashing square off.

--- alex.el
+++ alex.el
@@ -7,13 +7,7 @@
  '(load-home-init-file t t))
 
 (prefer-coding-system 'utf-8)
-
-;;(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-;; '(default ((t (:stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "apple" :family "Monaco")))))
+(setq visible-bell nil)
 
 ;; Font
 (set-face-font 'default "-apple-inconsolata-medium-r-normal--18-0-72-72-m-0-iso10646-1")
