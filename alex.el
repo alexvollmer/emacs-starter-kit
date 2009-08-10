8719faf50c98f1ac3b24219e9cf2f1775df8cf49
Alex Vollmer <alex@evri.com>
2009-08-10 16:24:10 -0700

    Added erlang-mode.

--- alex.el
+++ alex.el
@@ -142,3 +142,10 @@
 ;; tramp
 (setq tramp-default-method "ssh")
 (setq shell-prompt-pattern "^.*~: $")
+
+;; erlang
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/erlang-mode"))
+(setq erlang-root-dir "/usr/local/otp")
+(setq exec-path (cons "/usr/local/otp/bin" exec-path))
+(require 'erlang-start)
+(add-to-list 'auto-mode-alist '("\\.erl$" . erlang-mode))
